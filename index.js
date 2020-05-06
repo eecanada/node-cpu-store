1b39bd19711eb2d481405bed1175cbafaea975be
Eder Canada <canada.eder@gmail.com>
2020-05-05 20:25:42 -0700

    set up server

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
+const http = require('http')
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8')
 const laptopData = JSON.parse(json)
+
 console.log(laptopData)
 
 
+const server = http.createServer((req,res)=>{
+  console.log('server accessed')
+}) 
+
+server.listen(1337, '127.0.0.1', ()=>{
+  console.log('listening for request ')
+})
