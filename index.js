26dff323ddd5f6b3cf888ec438849959129a1889
Eder Canada <canada.eder@gmail.com>
2020-05-05 22:04:24 -0700

    correct routing

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ const laptopData = JSON.parse(json) // goes from json to javascript object
 
 const server = http.createServer((req,res)=>{
   // this is a header for the res.end response
-  const pathName = url.parse(req.url, true).pathname // true, so the query is parsed into an object, ROUTING
+  const pathName = url.parse(req.url,true).pathname // true, so the query is parsed into an object, ROUTING
+  const id  = url.parse(req.url,true).query.id //query id from url
+
 
   if(pathName === '/products' || pathName === '' ){
-    res.writeHead(200, {'Content-type': 'text/html '})
+    res.writeHead(200, {'Content-type': 'text/html'})
     res.end ('this is the products page')
   } 
   
-  else if (pathName === '/laptop'){
+  else if (pathName === '/laptop' && id < laptopData.length){
      res.writeHead(200, {'Content-type':'text/html'})
-     res.end(`this is the laptop's page`)
+     res.end(`this is the LAPTOP page for the laptop ${id}!`)
   }
   else {
     res.writeHead(400, {'Content-type': 'text/html'})
