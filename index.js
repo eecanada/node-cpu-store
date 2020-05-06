d577efa471098791997da2a558235d812d390625
Eder Canada <canada.eder@gmail.com>
2020-05-05 20:47:15 -0700

    set up routing

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url') //routing
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8')
-const laptopData = JSON.parse(json)
-
-console.log(laptopData)
+const laptopData = JSON.parse(json) // goes from json to javascript object
 
 
 const server = http.createServer((req,res)=>{
   // this is a header for the res.end response
+  const pathName = url.parse(req.url, true).pathname // true, so the query is parsed into an object
+
   res.writeHead(200, {'Content-type': 'text/html '})
   res.end ('this is the response')
 }) 
 
 server.listen(1337, '127.0.0.1', ()=>{
-  console.log('listening for request ')
+  console.log('listening for request')
 })
 
