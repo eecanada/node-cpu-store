7394b418b421f354bef13cc4368cd1f9c6a54d1d
Eder Canada <canada.eder@gmail.com>
2020-05-05 21:48:58 -0700

    set up routes for products, laptop, and 404

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ const laptopData = JSON.parse(json) // goes from json to javascript object
 
 const server = http.createServer((req,res)=>{
   // this is a header for the res.end response
-  const pathName = url.parse(req.url, true).pathname // true, so the query is parsed into an object
+  const pathName = url.parse(req.url, true).pathname // true, so the query is parsed into an object, ROUTING
 
-  res.writeHead(200, {'Content-type': 'text/html '})
-  res.end ('this is the response')
+  if(pathName === '/products' || pathName === '' ){
+    res.writeHead(200, {'Content-type': 'text/html '})
+    res.end ('this is the products page')
+  } 
+  
+  else if (pathName === '/laptop'){
+     res.writeHead(200, {'Content-type':'text/html'})
+     res.end(`this is the laptop's page`)
+  }
+  else {
+    res.writeHead(400, {'Content-type': 'text/html'})
+    res.end('Url was not found on the server')
+  }
+
 }) 
 
 server.listen(1337, '127.0.0.1', ()=>{
