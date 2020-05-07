3d90709b4ad4d90a038d93de90d5a0a7867cb0b3
Eder Canada <canada.eder@gmail.com>
2020-05-06 22:31:58 -0700

    fix screen title

--- index.js
+++ index.js
@@ -12,22 +12,17 @@ const server = http.createServer((req,res)=>{
   const id  = url.parse(req.url,true).query.id //query id from url
 
 
+
   if(pathName === '/products' || pathName === '' ){
     res.writeHead(200, {'Content-type': 'text/html'})
-    res.end ('this is the products page')
+    
   } 
   
   else if (pathName === '/laptop' && id < laptopData.length){
      res.writeHead(200, {'Content-type':'text/html'})
      fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf-8', (err, data)=>{
         const laptop = laptopData[id]
-        let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName) 
-        output = output.replace(/{%IMAGE%}/g, laptop.image)
-        output = output.replace(/{%PRICE%}/g, laptop.price)
-        output = output.replace(/{%CPU%}/g, laptop.cpu)
-        output = output.replace(/{%STORAGE%}/g, laptop.storage)
-        output = output.replace(/{%RAM%}/g, laptop.ram)
-        output = output.replace(/{%DESCRIPTION%}/g, laptop.description)
+        const output = replaceTemplate(data, laptop)
         res.end(output)
      })
   }
@@ -42,3 +37,16 @@ server.listen(1337, '127.0.0.1', ()=>{
   console.log('listening for request')
 })
 
+
+function replaceTemplate(originalHtml,laptop){
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName) 
+  output = output.replace(/{%IMAGE%}/g, laptop.image)
+  output = output.replace(/{%PRICE%}/g, laptop.price)
+  output = output.replace(/{%SCREEN%}/g, laptop.screen)
+  output = output.replace(/{%CPU%}/g, laptop.cpu)
+  output = output.replace(/{%STORAGE%}/g, laptop.storage)
+  output = output.replace(/{%RAM%}/g, laptop.ram) 
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description)
+  output = output.replace(/{%ID%}/g, laptop.id)
+  return output
+}
